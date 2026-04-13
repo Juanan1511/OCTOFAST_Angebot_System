df4e15b4a63359f811a1fe47f6ed71b21c1d0350
Juanan1511 <jacerda1511@gmail.com>
2026-04-13 16:31:31 +0200

    feat: estructura inicial Spring Boot con configuración SQLite

---
+++ src/main/java/com/octofast/angebot_system/AngebotSystemApplication.java
@@ -0,0 +1,13 @@
+package com.octofast.angebot_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngebotSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AngebotSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/octofast/angebot_system/AngebotSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octofast.angebot_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngebotSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
